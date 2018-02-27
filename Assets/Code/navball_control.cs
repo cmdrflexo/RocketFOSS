37771285950a334774596bc9071ab6d09e39a073
Grady Lorenzo <gradylorenzo@gmail.com>
2018-02-27 17:07:20 -0500

    2.27.2018.17.07
    Grady Lorenzo
    + RCS and SAS indicators

--- Assets/Code/navball_control.cs
+++ Assets/Code/navball_control.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 using UnityEngine.UI;
 public class navball_control : MonoBehaviour {
 
+    private Quaternion navballRotation;
     private float throttlePosition;
     private int velocity;
     private int altitude;
+    public bool rcs = false;
+    public bool sas = false;
+
+    private GameObject navballCameras;
     private GameObject throttleFill;
     private GameObject velocityReadout;
     private GameObject altitudeReadout;
+    private GameObject rcsIndicator;
+    private GameObject sasIndicator;
 
     //Public method to set throttle position display
 	public void setNewThrottlePosition(float pos)
@@ -30,19 +37,59 @@ public class navball_control : MonoBehaviour {
         altitude = Convert.ToInt32(alt);
     }
 
+    //public method to set navball rotation
+    public void setNewNavballRotation (Quaternion rot)
+    {
+        navballRotation = rot;
+    }
+
+    public void setNewRcsState (bool state)
+    {
+        rcs = state;
+    }
+
+    public void setNewSasState (bool state)
+    {
+        sas = state;
+    }
+
     private void Start()
     {
         //Find UI components
+        navballCameras = transform.Find("navball_cameras").gameObject;
         throttleFill = transform.Find("throttle_fill").gameObject;
         velocityReadout = transform.Find("navball_vel").gameObject;
         altitudeReadout = transform.Find("navball_alt").gameObject;
+        rcsIndicator = transform.Find("navball_rcs").gameObject;
+        sasIndicator = transform.Find("navball_sas").gameObject;
     }
 
     private void Update()
     {
         //Update all meters
+        navballCameras.transform.rotation = navballRotation;
         throttleFill.GetComponent<Image>().fillAmount = throttlePosition;
         velocityReadout.GetComponent<Text>().text = velocity.ToString();
         altitudeReadout.GetComponent<Text>().text = altitude.ToString();
+
+        //RCS Indicator
+        if (rcs)
+        {
+            rcsIndicator.GetComponent<Text>().text = "<color=#96FFACFF>RCS</color>";
+        }
+        else
+        {
+            rcsIndicator.GetComponent<Text>().text = "<color=#000000FF>RCS</color>";
+        }
+
+        //SAS Indicator
+        if (sas)
+        {
+            sasIndicator.GetComponent<Text>().text = "<color=#96C2FFFF>SAS</color>";
+        }
+        else
+        {
+            sasIndicator.GetComponent<Text>().text = "<color=#000000FF>SAS</color>";
+        }
     }
 }
