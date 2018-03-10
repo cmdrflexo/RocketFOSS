dd5afb49bc58535f61ce7dfc12e5e756e3ca37f1
flexo <f37314b1@opayq.com>
2018-03-10 01:28:13 -0500

    Add files via upload

---
+++ Assets/RFOSSCore/PlanetGen/Scripts/PlanetFace.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using UnityEngine;
+
+namespace RFOSSCore.PlanetGen {
+	public class PlanetFace : MonoBehaviour {
+
+		public PlanetSide side;
+		public Vector2Int coordinates;
+		public Vector3[] corners;
+		public Vector3 centerPoint;
+		public Vector3[] vertices;
+		public int LOD;
+		int previousLOD;
+
+		public MeshRenderer meshRenderer;
+		public MeshFilter meshFilter;
+		public MeshCollider meshCollider;
+		
+		public Mesh[] meshes = new Mesh[9];
+		MeshUtility meshUtility = new MeshUtility();
+		
+		private void Start() {
+			meshes = new Mesh[8];
+			meshFilter = gameObject.AddComponent<MeshFilter>();
+			meshRenderer = gameObject.AddComponent<MeshRenderer>();
+			meshCollider = gameObject.AddComponent<MeshCollider>();
+			CreateVertices();
+			UpdateMesh(true);
+			//CreateAllMeshes();
+		}
+
+		private void Update() {
+			if(LOD != previousLOD) { // add check for neighbor LOD changes to trigger update
+				previousLOD = LOD;
+				UpdateMesh(true); // initialize true, until edge fans can get updated
+			}
+		}
+
+		private void CreateVertices() {
+			int vSize = (int)Mathf.Pow(2, LOD + 1) + 1;
+			vertices = new Vector3[vSize * vSize];
+			for(int y = 0, i = 0; y < vSize; y++)
+				for(int x = 0; x < vSize; x++, i++) {
+					float xProgress = x / (vSize - 1f);
+					float yProgress = y / (vSize - 1f);
+					vertices[i] = Vector3.Lerp(
+						Vector3.Lerp(corners[0], corners[1], yProgress),
+						Vector3.Lerp(corners[3], corners[2], yProgress),
+						xProgress
+					);
+				}
+			vertices = side.meshUtility.SpherizeFace(vertices, side.planet.radius);
+			centerPoint = vertices[vertices.Length / 2];
+			vertices = side.meshUtility.AddHeightData(vertices, GetHeightData(), 1);
+		}
+
+		private float[,] GetHeightData() {
+			int heightDataSize = side.heightData.GetLength(0);
+			int sideSize = side.faces.GetLength(0);
+			int vSize = (int)Mathf.Pow(2, LOD + 1) + 1;
+			int facePatchSize = heightDataSize / sideSize;
+
+			float[,] faceHeightData = new float[vSize, vSize];
+
+			for(int y = 0; y < vSize; y++) {
+				for(int x = 0; x < vSize; x++) {
+					faceHeightData[x, y] = side.heightData[
+						(int)((facePatchSize * coordinates.x) + (x * (facePatchSize / (vSize + 1f)))),
+						(int)((facePatchSize * coordinates.y) + (y * (facePatchSize / (vSize + 1f))))
+					];
+				}
+			}
+			return faceHeightData;
+		}
+
+		private void CreateAllMeshes() {
+			for(LOD = 0; LOD < meshes.Length; LOD++) {
+				CreateVertices();
+				meshes[LOD] = meshUtility.GetFaceTriangles(vertices, this);
+				meshes[LOD].RecalculateNormals();
+			}
+			LOD = 0;
+			meshFilter.mesh = meshes[0];
+			meshRenderer.material = side.planet.defaultMaterial;
+			//meshRenderer.material.color = new Color(LOD * 0.2f, 0, 1 - LOD * 0.2f, 1);
+		}
+
+		private void UpdateMesh(bool initialize = false) {
+			if(initialize || meshes[LOD] == null) {
+				CreateVertices();
+				meshes[LOD] = meshUtility.GetFaceTriangles(vertices, this);
+				meshes[LOD].RecalculateNormals();
+				meshFilter.mesh = meshes[LOD];
+				meshRenderer.material = side.planet.defaultMaterial;
+				//meshRenderer.material.color = new Color(LOD * 0.2f, 0, 1 - LOD * 0.2f, 1);
+			} else {
+				meshFilter.mesh = meshes[LOD];
+			}
+			CreateCollider();
+		}
+
+		public bool disableColliders = true;
+		private void CreateCollider() {
+			if(!disableColliders && LOD >= 6) {
+				meshCollider.enabled = true;
+				meshCollider.sharedMesh = meshes[LOD];
+			} else {
+				meshCollider.enabled = false;
+			}
+		}
+	}
+}
---
+++ Assets/RFOSSCore/PlanetGen/Scripts/PlanetManager.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+
+namespace RFOSSCore.PlanetGen {
+	public class PlanetManager : MonoBehaviour {
+
+		BodyInfo planetInfo = new BodyInfo();
+		PlanetGenerator gen = new PlanetGenerator();
+		PlanetSide[] sides = new PlanetSide[6];
+
+		public GameObject cam;
+
+		public float[] LODThresholds = new float[7];
+
+		public Material defaultMaterial;
+
+		private void Start() {
+			CreatePlanet();
+			
+			//GetHeightDataTEST();
+		}
+
+		private void GetHeightDataTEST() {
+			float[,] heightData = GetHeightData();
+			int heightDataSize = 1000;
+			int sideSize = 10;
+			int vSize = 8;
+			Vector2 faceCoordinates = new Vector2(6, 8);
+			int facePatchSize = heightDataSize / sideSize / vSize;
+
+			float scaling = sideSize / vSize;
+			Vector2 offset = faceCoordinates * scaling;
+
+			float[,] faceHeightData = new float[vSize, vSize];
+			
+			string printOut = string.Empty;
+			for(int y = 0; y < vSize; y++) {
+				string line = string.Empty;
+				for(int x = 0; x < vSize; x++) {
+					
+					//print("offset: " + offset);
+					//print(
+					//	"x * (int)offset.x: "   + (facePatchSize * x + (int)offset.x * x) +
+					//	", y * (int)offset.y: " + (facePatchSize * y + (int)offset.y * y)
+					//);
+
+					faceHeightData[x, y] = heightData[
+						facePatchSize * x + (int)offset.x * x,
+						facePatchSize * y + (int)offset.y * y
+					];
+
+					//line += "[" + faceHeightData[x, y] + "]";
+
+				}
+				//printOut = line + "\n" + printOut;
+			}
+			//print(printOut);
+		}
+
+		private void Update() {
+			foreach(PlanetSide side in sides)
+				foreach(PlanetFace face in side.faces) {
+					//face.LOD = 4;
+					float distance = Vector3.Distance(cam.transform.position, face.centerPoint);
+					face.LOD = 0;
+					for(int i = 0; i < LODThresholds.Length; i++)
+						if(distance < LODThresholds[i])
+							face.LOD = i + 1;
+				}
+		}
+
+		private void CreatePlanet() {
+			planetInfo.name = "Planet";
+			planetInfo.sideResolution = 10;
+			planetInfo.radius = 100;
+			planetInfo.gameObject = new GameObject(planetInfo.name);
+			planetInfo.defaultMaterial = defaultMaterial;
+
+			float[,] heightData = GetHeightData();
+
+			//sides = new PlanetSide[1];
+			sides[0] = new PlanetSide(this, planetInfo, new Vector3(  0,    0,  0), heightData);
+			sides[1] = new PlanetSide(this, planetInfo, new Vector3(0, 90, 0), heightData);
+			sides[2] = new PlanetSide(this, planetInfo, new Vector3(0, 180, 0), heightData);
+			sides[3] = new PlanetSide(this, planetInfo, new Vector3(0, -90, 0), heightData);
+			sides[4] = new PlanetSide(this, planetInfo, new Vector3(90, 0, 1), heightData);
+			sides[5] = new PlanetSide(this, planetInfo, new Vector3(-90, 0, -1), heightData);
+		}
+
+		public Texture2D heightmap;
+		private float[,] GetHeightData() {
+			float[,] heightData = new float[heightmap.width, heightmap.width];
+			//print("heightmap.width: " + heightmap.width);
+			//print("(int)Mathf.Sqrt(heightData.GetLength(0): " + heightData.GetLength(0));
+			for(int y = 0; y <= heightmap.width - 1; y++)
+				for(int x = 0; x <= heightmap.width - 1; x++)
+					heightData[x, y] = heightmap.GetPixel(x, y).grayscale;
+			return heightData;
+		}
+
+		private void OnDrawGizmos() {
+			Gizmos.color = Color.green;
+			if(sides[0] != null)
+				foreach(Vector3 sideVertex in sides[0].vertices)
+					Gizmos.DrawWireSphere(sideVertex, 0.5f);
+			//if(sides[0].faces[0, 0] != null) {
+			//	PlanetFace face = sides[0].faces[0, 0];
+			//	int vSize = (int)Mathf.Pow(2, face.LOD + 1) + 1;
+			//	for(int y = 0, i = 0; y < vSize; y++)
+			//		for(int x = 0; x < vSize; x++, i++) {
+			//			face
+			//		}
+			//}
+		}
+	}
+}
